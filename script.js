573279d33e4781ff1ba97ee9546aa94afdd8195d
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-10 15:38:22 -0500

    Add fade/scroll effects

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 var form = document.querySelector('.js-form');
 
-form.addEventListener('submit', showSurvey);
+var jumpToSurvey = document.querySelector('.js-jump-to-survey');
 
-function showSurvey(event) {
+form.addEventListener('submit', handleFormSubmit);
+
+jumpToSurvey.addEventListener('click', handleScrollToSurvey);
+
+function handleFormSubmit(event) {
   event.preventDefault();
-  console.log(event);
-  var survey = document.querySelector('.survey');
+  var survey = document.querySelector('.js-survey');
+  survey.innerHTML = `
+  <iframe src="https://www.surveymonkey.com/r/Z9YDLVP" frameborder="0"></iframe>
+  `;
 
-  var success = document.querySelector('.thank-you');
+  fadeIn(survey);
 
-  form.style.display = 'none';
-  survey.style.display = 'block';
-  success.style.display = 'block';
+  showThanksAndHideEmail();
+}
 
-  Velocity(survey, "scroll", {duration: 500});
+function showThanksAndHideEmail() {
+  var success = document.querySelector('.js-success');
+
+  fadeIn(success);
+  fadeOut(form);
+}
+
+function handleScrollToSurvey() {
+  var container = document.querySelector('.js-survey-container');
+
+  Velocity(container, 'scroll', {
+    duration: 800
+  });
+}
+
+function fadeIn(element) {
+  Velocity(element, 'fadeIn', { display: 'block' });
+}
+
+function fadeOut(element) {
+  Velocity(element, 'fadeOut', {
+    display: 'none'
+  });
 }
