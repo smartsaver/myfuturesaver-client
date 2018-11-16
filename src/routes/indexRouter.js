4930441bb886f259470da6b8210ab14fb1ec3d26
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-16 16:52:04 -0500

    Add Header Images

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -6,9 +6,10 @@ router.get('/', (_, res) => {
   const { staticUrl } = expressConfig;
   res.render('index/index', {
     logo: `${staticUrl}/images/logo-small.png`,
+    headerImage: `${staticUrl}/images/header-image-fade-sides.png`,
     script: `${staticUrl}/index/script.js`,
     style: `${staticUrl}/index/style.css`
-  });
+  })
 });
 
 module.exports = router;
