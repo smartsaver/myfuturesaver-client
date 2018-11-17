153c3b0641cc95137e21a08824652668d558b463
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-16 20:16:57 -0500

    Add Mobile breakpoints for background image

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -6,7 +6,8 @@ router.get('/', (_, res) => {
   const { staticUrl } = expressConfig;
   res.render('index/index', {
     logo: `${staticUrl}/images/logo-small.png`,
-    headerImage: `${staticUrl}/images/header-image-fade-sides.png`,
+    headerImageDesktop: `${staticUrl}/images/header-image-fade-sides.png`,
+    headerImageMobile: `${staticUrl}/images/header-image-wide.jpg`,
     script: `${staticUrl}/index/script.js`,
     style: `${staticUrl}/index/style.css`
   })
