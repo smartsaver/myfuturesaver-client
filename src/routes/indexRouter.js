5a7122540025e5fc0216c5dddcd7230c0e0afb3f
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-16 23:43:14 -0500

    Make diff style for mobile and desktop and fix horizontal scrolling
    fix horizontal scrolling for overlays

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -6,8 +6,6 @@ router.get('/', (_, res) => {
   const { staticUrl } = expressConfig;
   res.render('index/index', {
     logo: `${staticUrl}/images/logo-small.png`,
-    headerImageDesktop: `${staticUrl}/images/header-image-fade-sides.png`,
-    headerImageMobile: `${staticUrl}/images/header-image-wide.jpg`,
     script: `${staticUrl}/index/script.js`,
     style: `${staticUrl}/index/style.css`
   })
