7a04c86d2a118d8bcaf93d24d7331a75491d29a5
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-18 23:23:24 -0500

    Add new section

--- src/routes/indexRouter/content.js
+++ src/routes/indexRouter/content.js
@@ -2,15 +2,11 @@ const processMarkdown = require('../../libs/marked');
 
 const content = {
   claim: processMarkdown(`
-### Two things you can claim for your children
-
 1. __Claim you child's Canada Learning Bond (CLB)__ - a federal grant of $500 to $2,000 to eligible families to help with the cost of a child's education after high school. 
 2. __Participate in this pilot__ - you could receive an extra $500 from SmartSAVER to eligible families! (Please note: A family can receive $500 only once, whether they are applying for one or more children.)
 `),
 
   whoCanParticipate: processMarkdown(`
-### Who can participate?
-
 __Child/Children:__
 * Born on or after January 1, 2004.
 * Has a Social Insurance Number. 
