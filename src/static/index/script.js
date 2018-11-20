a04b93bc83529491a7e9c873999c5bc8b55edae2
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-20 00:29:24 -0500

    Add Axios, Add effects

--- src/static/index/script.js
+++ src/static/index/script.js
@@ -1,25 +1,49 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+function Effects() {
+  function scrollTo(containerName) {
+    $(containerName).velocity('scroll', {
+      duration: 800
+    });
+  }
+
+  function fadeIn(elementName) {
+    $(elementName).velocity('fadeIn', { display: 'block' });
+  }
+
+  function fadeOut(elementName) {
+    $(elementName).velocity('fadeOut', {
+      display: 'none'
+    });
+  }
+
+  return {
+    scrollTo,
+    fadeIn,
+    fadeOut
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function runScripts() {
+  var effects = new Effects();
+  var navLinkList = document.querySelectorAll('.js-nav-link');
+
+  // enable popover
+  $(function() {
+    $('[data-toggle="popover"]').popover();
+  });
+
+  navLinkList.forEach(handleNavLink);
+
+  function handleNavLink(navLink) {
+    navLink.addEventListener('click', handleNavLinkClick);
+
+    function handleNavLinkClick(event) {
+      event.stopPropagation();
+      event.preventDefault();
+      var containerId = event.target.getAttribute('href');
+      //smooth scroll
+      effects.scrollTo(containerId);
+      // then push history
+      history.pushState({}, "", containerId);
+    }
+  }
 });
-
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-
-function handleScrollToSurvey() {
-  var container = document.querySelector('.js-survey-container');
-
-  Velocity(container, 'scroll', {
-    duration: 800
-  });
-}
-
-function fadeIn(element) {
-  Velocity(element, 'fadeIn', { display: 'block' });
-}
-
-function fadeOut(element) {
-  Velocity(element, 'fadeOut', {
-    display: 'none'
-  });
-}
