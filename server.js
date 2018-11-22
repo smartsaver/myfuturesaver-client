fafff6d87dc054fa3917f1e779479c092d9700a9
jcunanan05 <j_cunanan05@yahoo.com>
2018-11-21 23:37:39 -0500

    prod config

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const appConfig = require('./config/expressConfig');
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 const indexRouter = require('./src/routes/indexRouter');
-const emailRouter = require('./src/routes/emailRouter');
+// const emailRouter = require('./src/routes/emailRouter');
 const downloadRouter = require('./src/routes/downloadRouter');
-
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
-}
-console.log(process.env.NODE_ENV);
 
 // load configurations
 appConfig.loadConfigs(app);
 
 // routes
 app.use('/', indexRouter);
-app.use('/api/email', emailRouter);
+// app.use('/api/email', emailRouter);
 app.use('/download', downloadRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
