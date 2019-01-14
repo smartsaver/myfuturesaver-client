6ac71160b8a88beb5efa5bfc02bfda3e348f8d45
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-01-13 22:56:20 -0500

    Change Component Name
    Changed SurveyMonkeyForm because there'll be upcoming survey monkey form

--- src/components/sections/ListOfForms/index.js
+++ src/components/sections/ListOfForms/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import TermsOfAgreementForm from './TermsOfAgreement'
 import EmailForm from './EmailForm'
-import SurveyMokeyForm from './SurveyMonkeyForm'
+import CLBSurveyForm from './ClbSurveyForm'
 import RespForm from './RespForm'
 
 class ListOfForms extends Component {
@@ -25,7 +25,7 @@ class ListOfForms extends Component {
           onSubmit={this.handleTermsOfAgreementSubmit}
         />
         <EmailForm isEnabled={isTermsOfAgreementAccepted} />
-        <SurveyMokeyForm isEnabled={isTermsOfAgreementAccepted} />
+        <CLBSurveyForm isEnabled={isTermsOfAgreementAccepted} />
         <RespForm isEnabled={isTermsOfAgreementAccepted} />
       </div>
     )
