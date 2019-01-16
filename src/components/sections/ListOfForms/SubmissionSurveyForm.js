3cfd5695975df73b9c52651a50cf62a920617589
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-01-16 04:39:43 -0500

    Add I'm ready to submit button on Submission Section

--- src/components/sections/ListOfForms/SubmissionSurveyForm.js
+++ src/components/sections/ListOfForms/SubmissionSurveyForm.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SurveyForm from '../../SurveyForm'
 import Section from '../../Section'
 import submissionContent from '../../../content/submissionContent'
 
-const SubmissionSurveyForm = ({ isEnabled }) => {
-  const { heading, intro, url, footer } = submissionContent
-  return (
-    <Section
-      name="submission-form"
-      className="SubmissionSurveyForm Section--blue"
-    >
-      <header>
-        <h2 className="title">{heading}</h2>
-      </header>
-      <div className="content" dangerouslySetInnerHTML={{ __html: intro }} />
-      <SurveyForm isEnabled={isEnabled}>
-        <iframe title="Submission" src={url} />
-      </SurveyForm>
-      <footer
-        className="content"
-        dangerouslySetInnerHTML={{ __html: footer }}
-      />
-    </Section>
-  )
+class SubmissionSurveyForm extends Component {
+  state = {
+    isFormEnabled: false,
+  }
+
+  setFormEnabled(booleanValue) {
+    if (typeof booleanValue !== 'boolean') return
+    this.setState(() => ({ isFormEnabled: booleanValue }))
+  }
+
+  handleButtonClick = () => {
+    this.setFormEnabled(true)
+  }
+
+  render() {
+    const { heading, intro, url, footer } = submissionContent
+    const isFormEnabled = this.props.isEnabled || this.state.isFormEnabled
+    const { handleButtonClick } = this
+    return (
+      <Section
+        name="submission-form"
+        className="SubmissionSurveyForm Section--blue"
+      >
+        <header>
+          <h2 className="title">{heading}</h2>
+        </header>
+        <div className="content" dangerouslySetInnerHTML={{ __html: intro }} />
+        <button className="button is-warning" onClick={handleButtonClick}>
+          I read the terms and ready to submit
+        </button>
+        <SurveyForm isEnabled={isFormEnabled}>
+          <iframe title="Submission" src={url} />
+        </SurveyForm>
+        <footer
+          className="content"
+          dangerouslySetInnerHTML={{ __html: footer }}
+        />
+      </Section>
+    )
+  }
 }
 
 export default SubmissionSurveyForm
