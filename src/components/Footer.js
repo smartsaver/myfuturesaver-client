f6537858bfcef3b01e15cc227b5bb2724b248e38
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-01-16 17:19:46 -0500

    Add Footer Content
    Add Terms and Privacy Policy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,37 @@
 import React from 'react'
 import logo from '../images/logo-small.png'
-import { footer } from '../content'
+import footerContent from '../content/footerContent'
 import './Footer.css'
 
 const Footer = () => {
+  const { intro, contact, otherLinks, footer } = footerContent
   return (
-    <footer id="contact" className="footer Footer--blue">
+    <footer id="contact" className="footer Footer Footer--blue">
       <div className="container">
         <div className="columns">
           <div className="column is-half">
             <img src={logo} alt="SmartSAVER" />
-            <p>Copyright SmartSAVER.org © 2018. All Rights Reserved.</p>
-          </div>
-          <div className="column is-half">
-            <h3 className="title is-3">Contact Us</h3>
             <div
               className="content"
-              dangerouslySetInnerHTML={{ __html: footer.footer }}
+              dangerouslySetInnerHTML={{ __html: intro }}
             />
+            <div
+              className="title is-6 is-marginless"
+              dangerouslySetInnerHTML={{ __html: otherLinks }}
+            />
+            <div
+              className="content is-marginless"
+              dangerouslySetInnerHTML={{ __html: footer }}
+            />
+          </div>
+          <div className="column is-half">
+            <div>
+              <h3 className="title is-3">Contact Us</h3>
+              <div
+                className="content"
+                dangerouslySetInnerHTML={{ __html: contact }}
+              />
+            </div>
           </div>
         </div>
       </div>
