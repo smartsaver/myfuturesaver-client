bb1f91e0b2e16b4a9bd6fd004555ac8d5e2ae943
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-26 12:30:07 -0500

    remove components

--- src/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Notification = ({ message = '', className, isHidden = false }) => {
-  return isHidden ? null : (
-    <div className={`notification ${className}`}>{message}</div>
-  )
-}
-
-export default Notification
