b0750bfce1738353b1896951dab1aa37ae5775d6
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-27 14:46:10 -0500

    Add Certificate Images
    Comment out whole index to focus on the certificate

--- src/pages-components/index/index.js
+++ src/pages-components/index/index.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import HomeLayout from './HomeLayout'
-import SEO from '../../components/Seo'
-import SmartSaverBanner from './SmartSaverBanner'
-import ClaimSection from './Claim'
-import ParticipateSection from './Participate'
-import ListOfFormsSection from './ListOfForms'
-import BmoSection from './Bmo'
-import WealthsimpleSection from './Wealthsimple'
-import NoSponsorshipSection from './NoSponorship'
-import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
-import RespUploadSection from './RespUpload'
-import StepsToApplySection from './StepsToApply'
-import FaqSection from './Faq'
+// import SEO from '../../components/Seo'
+// import SmartSaverBanner from './SmartSaverBanner'
+// import ClaimSection from './Claim'
+// import ParticipateSection from './Participate'
+// import ListOfFormsSection from './ListOfForms'
+// import BmoSection from './Bmo'
+// import WealthsimpleSection from './Wealthsimple'
+// import NoSponsorshipSection from './NoSponorship'
+// import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
+// import RespUploadSection from './RespUpload'
+// import StepsToApplySection from './StepsToApply'
+// import FaqSection from './Faq'
+
+// const IndexPage = () => {
+//   return (
+//     <HomeLayout>
+//       <SEO title="Home" keywords={[`resp`, `myfuturesaver`, `education`]} />
+//       <SmartSaverBanner />
+//       <ClaimSection />
+//       <ParticipateSection />
+//       <StepsToApplySection />
+//       <ListOfFormsSection />
+//       <BmoSection />
+//       <WealthsimpleSection />
+//       <NoSponsorshipSection />
+//       <SubmissionSurveyForm />
+//       <RespUploadSection />
+//       <FaqSection />
+//     </HomeLayout>
+//   )
+// }
 
 const IndexPage = () => {
   return (
     <HomeLayout>
-      <SEO title="Home" keywords={[`resp`, `myfuturesaver`, `education`]} />
-      <SmartSaverBanner />
-      <ClaimSection />
-      <ParticipateSection />
-      <StepsToApplySection />
-      <ListOfFormsSection />
-      <BmoSection />
-      <WealthsimpleSection />
-      <NoSponsorshipSection />
-      <SubmissionSurveyForm />
-      <RespUploadSection />
-      <FaqSection />
+      <h2 className="title">Hello</h2>
     </HomeLayout>
   )
 }
