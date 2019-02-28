ce03caefa95fa2c1a15f23df5be3d10a3edb4b72
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-28 18:08:57 -0500

    Clean up index
    Remove Context Provider

--- src/pages-components/certificate-app/index.js
+++ src/pages-components/certificate-app/index.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import { Section } from './components'
 import CertificateFormContainer from './state/CertificateFormContainer'
-import CertificateContext, {
-  defaultCertificateState,
-} from './state/CertificateContext'
+
+import AppContainer from './state/AppContainer'
 
 const CertificateApp = () => {
   return (
-    <CertificateContext.Provider value={defaultCertificateState}>
-      <Section>
-        <h2 className="title">MyFutureSaver Certificate</h2>
-        <div className="columns">
-          <div className="column is-5">
-            <CertificateFormContainer />
-          </div>
-          <div className="column is-7">Preview Here</div>
+    <Section>
+      <h2 className="title">MyFutureSaver Certificate</h2>
+      <div className="columns">
+        <div className="column is-5">
+          <CertificateFormContainer />
         </div>
-      </Section>
-    </CertificateContext.Provider>
+        <div className="column is-7">Preview Here</div>
+      </div>
+    </Section>
   )
 }
 
-export default CertificateApp
+export default AppContainer(CertificateApp)
