93979035e93b9ed48fa2c682474563e55eaf1911
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-03-04 17:27:28 -0500

    Add Preview component
    Pdf preview component

---
+++ src/pages-components/certificate-app/components/Preview.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import * as jsPdf from 'jspdf'
+import style from './Preview.module.css'
+
+const Preview = ({ sender = '', recepient = '', message = '' }) => {
+  const doc = new jsPdf({
+    orientation: 'landscape',
+    format: 'letter',
+  })
+
+  doc.text(sender, 20, 20)
+  doc.text(recepient, 20, 40)
+  doc.text(message, 20, 50)
+
+  return (
+    <embed
+      className={style.PreviewFrame}
+      src={doc.output('datauristring')}
+      title="Pdf Preview"
+    />
+  )
+}
+
+export default Preview
