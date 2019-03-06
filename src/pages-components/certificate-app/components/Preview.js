10110bf05d170e56d0cb659291c3caad012074d4
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-03-05 23:31:52 -0500

    try out fonts

--- src/pages-components/certificate-app/components/Preview.js
+++ src/pages-components/certificate-app/components/Preview.js
@@ -2,23 +2,22 @@ import React from 'react'
 import jsPdf from 'jspdf'
 import style from './Preview.module.css'
 
-function previewPdf({ sender, recepient, message }) {
-  const doc = new jsPdf({
-    orientation: 'landscape',
-    format: 'letter',
-  })
-
-  doc.text(sender, 20, 20)
-  doc.text(recepient, 20, 40)
-  doc.text(message, 20, 50)
-  return doc.output('datauristring')
-}
-
 const Preview = ({ sender = '', recepient = '', message = '' }) => {
+  function previewPdf() {
+    const doc = new jsPdf({
+      orientation: 'landscape',
+      format: 'letter',
+    })
+    doc.setFontSize(30)
+    doc.text(recepient, 100, 100)
+    doc.text(message, 100, 140)
+    doc.text(sender, 100, 180)
+    return doc.output('datauristring')
+  }
   return (
     <embed
       className={style.PreviewFrame}
-      src={previewPdf({ sender, recepient, message })}
+      src={previewPdf()}
       type="application/pdf"
     />
   )
