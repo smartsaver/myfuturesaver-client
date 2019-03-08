9b875c6f0094b4a41ffcae4ad3328f11bd4e6450
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-03-07 21:26:17 -0500

    Include Certificate app  in index

--- src/pages-components/index/index.js
+++ src/pages-components/index/index.js
@@ -1,42 +1,35 @@
 import React from 'react'
 import HomeLayout from './HomeLayout'
 import CertificateApp from '../certificate-app'
-// import SEO from '../../components/Seo'
-// import SmartSaverBanner from './SmartSaverBanner'
-// import ClaimSection from './Claim'
-// import ParticipateSection from './Participate'
-// import ListOfFormsSection from './ListOfForms'
-// import BmoSection from './Bmo'
-// import WealthsimpleSection from './Wealthsimple'
-// import NoSponsorshipSection from './NoSponorship'
-// import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
-// import RespUploadSection from './RespUpload'
-// import StepsToApplySection from './StepsToApply'
-// import FaqSection from './Faq'
-
-// const IndexPage = () => {
-//   return (
-//     <HomeLayout>
-//       <SEO title="Home" keywords={[`resp`, `myfuturesaver`, `education`]} />
-//       <SmartSaverBanner />
-//       <ClaimSection />
-//       <ParticipateSection />
-//       <StepsToApplySection />
-//       <ListOfFormsSection />
-//       <BmoSection />
-//       <WealthsimpleSection />
-//       <NoSponsorshipSection />
-//       <SubmissionSurveyForm />
-//       <RespUploadSection />
-//       <FaqSection />
-//     </HomeLayout>
-//   )
-// }
+import SEO from '../../components/Seo'
+import SmartSaverBanner from './SmartSaverBanner'
+import ClaimSection from './Claim'
+import ParticipateSection from './Participate'
+import ListOfFormsSection from './ListOfForms'
+import BmoSection from './Bmo'
+import WealthsimpleSection from './Wealthsimple'
+import NoSponsorshipSection from './NoSponorship'
+import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
+import RespUploadSection from './RespUpload'
+import StepsToApplySection from './StepsToApply'
+import FaqSection from './Faq'
 
 const IndexPage = () => {
   return (
     <HomeLayout>
-      <CertificateApp />
+      <SEO title="Home" keywords={[`resp`, `myfuturesaver`, `education`]} />
+      <SmartSaverBanner />
+      <ClaimSection />
+      <ParticipateSection />
+      <StepsToApplySection />
+      <ListOfFormsSection />
+      <BmoSection />
+      <WealthsimpleSection />
+      <NoSponsorshipSection />
+      <SubmissionSurveyForm />
+      <RespUploadSection />
+      <CertificateApp sectionName="certificate" />
+      <FaqSection />
     </HomeLayout>
   )
 }
