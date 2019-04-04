9c136d8c4f9f73bff9a9a005c7519b458ad098d2
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-04-04 13:54:18 -0400

    Commented out Certificate App

--- src/pages-components/index/index.js
+++ src/pages-components/index/index.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import HomeLayout from './HomeLayout'
 import SEO from '../../components/Seo'
-// import SmartSaverBanner from './SmartSaverBanner'
-// import ClaimSection from './Claim'
-// import ParticipateSection from './Participate'
-// import ListOfFormsSection from './ListOfForms'
-// import BmoSection from './Bmo'
-// import WealthsimpleSection from './Wealthsimple'
-// import NoSponsorshipSection from './NoSponorship'
-// import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
-// import RespUploadSection from './RespUpload'
-// import StepsToApplySection from './StepsToApply'
-// import FaqSection from './Faq'
-import CertificateApp from '../certificate-app'
+import SmartSaverBanner from './SmartSaverBanner'
+import ClaimSection from './Claim'
+import ParticipateSection from './Participate'
+import ListOfFormsSection from './ListOfForms'
+import BmoSection from './Bmo'
+import WealthsimpleSection from './Wealthsimple'
+import NoSponsorshipSection from './NoSponorship'
+import SubmissionSurveyForm from './ListOfForms/SubmissionSurveyForm'
+import RespUploadSection from './RespUpload'
+import StepsToApplySection from './StepsToApply'
+import FaqSection from './Faq'
+// import CertificateApp from '../certificate-app'
 
 const IndexPage = () => {
   return (
     <HomeLayout>
       <SEO title="Home" keywords={[`resp`, `myfuturesaver`, `education`]} />
-      {/* <SmartSaverBanner />
+      <SmartSaverBanner />
       <ClaimSection />
       <ParticipateSection />
       <StepsToApplySection />
@@ -27,9 +27,9 @@ const IndexPage = () => {
       <WealthsimpleSection />
       <NoSponsorshipSection />
       <SubmissionSurveyForm />
-      <RespUploadSection /> */}
-      <CertificateApp sectionName="certificate" />
-      {/* <FaqSection /> */}
+      <RespUploadSection />
+      {/* <CertificateApp sectionName="certificate" /> */}
+      <FaqSection />
     </HomeLayout>
   )
 }
